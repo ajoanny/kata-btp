ee839ef66151c8e3501caa097d56fda929f36b7b
Karam Mohd <k.haytham.mohammed@octo.com>
2021-12-16 11:03:57 +0100

    wip

--- test/CLITest.js
+++ test/CLITest.js
@@ -76,4 +76,45 @@ describe('CLITest', function () {
 
         });
     });
+
+
+    describe('?', function () {
+        it('works', function () {
+
+            var output = stdout.inspectSync(function () {
+                // Given
+                var i = 0;
+                var prompt = () => {
+                    i++;
+                    return i > 1 ? 'quit' : 'help';
+                };
+
+                // Don't forget to decomment the line on CLI.js!!!!!
+                // If not decomment, the test won't work!!!!!
+                dacli = new CLI(prompt);
+
+                // When
+                dacli.main();
+            });
+
+            // Then
+            assert.deepEqual(
+                output,
+                [
+                    "\n--------------------------------------------------------------------------------\n",
+                    "          Welcome to Efficent Command System 2.0\n",
+                    "--------------------------------------------------------------------------------\n\n",
+                    "\nUnknown command\nThe available commands are the following : \n\n",
+                    "quit - quit the program\n",
+                    "order - create a new command\n",
+                    "help - displays the help\n",
+                    "\n",
+                    "\n--------------------------------------------------------------------------------\n",
+                    "          System stopped\n",
+                    "--------------------------------------------------------------------------------\n\n"
+                ]
+            );
+
+        });
+    });
 });
